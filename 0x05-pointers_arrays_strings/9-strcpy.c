1f8b97360ef8ee37d39c75e76a1810b8d331e147
Sassysuch <suchidera@gmail.com>
2023-10-17 17:01:15 -0700

    9-stropy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function name
+ * @dest: first parameter
+ * @src: second parameter
+ *
+ * Return: This return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
