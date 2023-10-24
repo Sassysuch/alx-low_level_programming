8f8028ef2b53d3a1b7d7bf8842a5fb5f77c0405f
Sassysuch <suchidera@gmail.com>
2023-10-24 15:52:31 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * set_string - The main function
+  *
+  * @s: Function parameter 1
+  *
+  * @to: Function parameter 2
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
+
