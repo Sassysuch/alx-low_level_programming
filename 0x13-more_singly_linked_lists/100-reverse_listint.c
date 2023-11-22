3407f57df8ed7296eb8742d6377ad3a4834bd799
Sassysuch <suchidera@gmail.com>
2023-11-21 20:01:35 -0800

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * reverse_listint - This defines function
+  * @head: To describe argument
+  * Return: what does it return?
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
